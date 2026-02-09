44ef09ec9e17b156bfda174a5d612e1deee4cedd
Pacoshao <52244664+Pacoshao@users.noreply.github.com>
2026-02-09 04:07:20 +0000

    env secret 清除

--- setenv.sh
+++
@@ -1,7 +0,0 @@
-export DB_HOST=mysql6.sqlpub.com
-export DB_PORT=3311
-export DB_USER=coffee_order
-export DB_PASSWORD=G3rpl3C93k8upIMu
-export DB_NAME=hkustgz_coffee_order
-export ADMIN_PASSWORD=hkustgz
-export NODE_ENV=production
