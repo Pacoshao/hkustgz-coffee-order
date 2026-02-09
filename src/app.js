bad9dab328ee0cf81f750d807920538f6159d228
Pacoshao <52244664+Pacoshao@users.noreply.github.com>
2026-02-09 04:09:56 +0000

    secret 清除大行动

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { getPool } = require('./db');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
